1636387275d62e07a60eee98a47f272faec81f95
Mario Flores Jr <m.flores4821@gmail.com>
2018-04-26 20:53:38 -0700

    route-dragon work

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -10,58 +10,68 @@ let mockId = null;
 beforeAll(() => server.start(testPort));
 afterAll(() => server.stop());
 
-describe('POST request to /api/dragon', () => {
-  test('should respond with creation of new dragon', () => superagent.post(`:${testPort}/api/dragon`)
-    .send(mockResource)
-    .then((res) => {
-      mockId = res.body.id;
-      expect(res.status).toEqual(201);
-    }));
-});
-
-describe('GET /api/v1/track', () => {
-  it('should respond with a previously created track', () => superagent.get(`:${testPort}/api/v1/track?id=${mockId}`)
-    .then((res) => {
-      expect(res.body.title).toEqual(mockResource.title);
-      expect(res.body.content).toEqual(mockResource.content);
-      expect(res.status).toEqual(200);
-    }));
-});
-
 describe('VALID request to the API', () => {
-  describe('POST /api/v1/track', () => {
-    it('should respond with status 201 and create a new track', () => superagent.post(`:${testPort}/api/v1/track`)
-      .send(mockResource)
-      .then((res) => {
-        mockID = res.body.id;
-        expect(res.body.title).toEqual(mockResource.title);
-        expect(res.body.content).toEqual(mockResource.content);
-        expect(res.status).toEqual(201);
-      }));
+  describe('POST /api/v1/dragon', () => {
+    it('should respond with status 201 and create a new dragon', () => {
+      return superagent.post(`:${testPort}/api/v1/dragon`)
+        .send(mockResource)
+        .then((res) => {
+          mockID = res.body.id;
+          expect(res.body.title).toEqual(mockResource.title);
+          expect(res.body.content).toEqual(mockResource.content);
+          expect(res.status).toEqual(201);
+        });
+    });
+  });
+
+  describe('GET /api/v1/dragon', () => {
+    it('should respond with a previously created dragon', () => {
+      return superagent.get(`:${testPort}/api/v1/dragon?id=${mockId}`)
+        .then((res) => {
+          expect(res.body.title).toEqual(mockResource.title);
+          expect(res.body.content).toEqual(mockResource.content);
+          expect(res.status).toEqual(200);
+        });
+    });
   });
 });
-describe('INVALID request to the API', () => {
-  describe('GET /api/v1/track', () => {
-    it('should err out with 404 status code for not sending anything', () => superagent.get(':5000/track')
-      .query({})
-      .then(() => {})
-      .catch((err) => {
-        expect(err.status).toEqual(404);
-        expect(err).toBeTruthy();
-      }));
-  });
-  it('should err out with 400 status code for not sending anything', () => superagent.get(':5000/track')
-    .query({})
-    .then(() => {})
-    .catch((err) => {
-      expect(err.status).toEqual(400);
-      expect(err).toBeTruthy();
-    }));
-}); it('should err out with 404 status code for not sending anything', () => superagent.get(':5000/track')
-  .query({})
-  .then(() => {})
-  .catch((err) => {
-    expect(err.status).toEqual(404);
-    expect(err).toBeTruthy();
-  }));
 
+describe('INVALID request to the API', () => {
+  describe('GET /api/v1/dragon', () => {
+    it('should err out with 404 status code for not sending anything', () => {
+      return superagent.get(':5000/dragon')
+        .then(() => {})
+        .catch((err) => {
+          expect(err.status).toEqual(404);
+          expect(err).toBeTruthy();
+        });
+    });
+    it('should err out with 400 status code for not sending anything', () => {
+      return superagent.get(':5000/dragon')
+        .query({})
+        .catch((err) => {
+          expect(err.status).toEqual(400);
+          expect(err).toBeTruthy();
+        });
+    }); 
+    it('should err out with 404 status code for not sending anything', () => {
+      return superagent.get(`:${testPort}/api/v1/note?id=`)
+        .query({})
+        .then(() => {})
+        .catch((err) => {
+          expect(err.status).toEqual(400);
+          expect(err).toBeTruthy();
+        });
+    });
+  });
+  describe('POST /api/v1/note', () => {
+    it('should err out with 400 when no route exists', () => {
+      return superagent.get(':5000/cowsayPage')
+        .query({})
+        .catch((err) => {
+          expect(err.status).toEqual(400);
+          expect(err).toBeTruthy();
+        });
+    });
+  });
+});
--- route/route-dragon.js
+++ route/route-dragon.js
@@ -27,6 +27,28 @@ module.exports = function routeDragon(router) {
     return undefined;
   });
 
+  router.post('/api/dragon/all', (req, res) => {
+    logger.log(logger.INFO, 'DRAGON-ROUTE - POST /api/dragon/all');
+
+    try {
+      const newDragon = new Dragon(req.body.title, req.body.content);
+      storage.create('Dragon', newDragon)
+        .then((dragon) => {
+          res.writeHead(201, { 'Content-Type': 'application/json' });
+          res.write(JSON.stringify(dragon));
+          res.end();
+          return undefined;
+        });
+    } catch (err) {
+      logger.log(logger.ERROR, `DRAGON-ROUTE - Bad request ${err}`);
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.write('Bad request for POST');
+      res.end();
+      return undefined;
+    }
+    return undefined;
+  });
+
   router.get('/api/v1/dragon', (req, res) => {
     if (!req.url.query.id) {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
@@ -36,12 +58,14 @@ module.exports = function routeDragon(router) {
     }
     storage.fetchOne('Dragon', req.url.query.id)
       .then((item) => {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
+        logger.log(logger.INFO, `in get: ${JSON.stringify(item)}`);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.write(JSON.stringify(item));
         res.end();
+        return undefined;
       })
       .catch((err) => {
-        logger.log(logger.ERROR, err, JSON.stringify(err));
+        logger.log(logger.ERROR, JSON.stringify(err));
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.write('Resource not found');
         res.end();
@@ -49,4 +73,50 @@ module.exports = function routeDragon(router) {
       });
     return undefined;
   });
+
+  router.omit('/api/dragon', (req, res) => {
+    logger.log('delete');
+    if (!req.url.query.id) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.write('No id - request requires an id');
+      res.end();
+      return undefined;
+    }
+    logger.log(logger.INFO, 'DRAGON-ROUTE - DELETE /api/dragon');
+
+    try {
+      storage.delete('Dragon', req.url.query.id)
+        .then((message) => {
+          res.writeHead(204, { 'Content-Type': 'text/plain'});
+          res.write(message);
+          res.end();
+          return undefined;
+        });
+    } catch (err) {
+      logger.log(logger.ERRO, `DRAGON-ROUTE - Bad Request ${err}`);
+      res.writeHead(400, { 'Content-Type': 'text/plain'});
+      res.write('Bad delete request');
+      res.end();
+      return undefined;
+    }
+    return undefined;
+  });
+  router.get('/api/dragons/all', (req, res) => {
+    storage.fetchAll('Dragon')
+      .then((items) => {
+        logger.log(logger.INFO, `in get: ${JSON.stringify(items)}`);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write(JSON.stringify(items));
+        res.end();
+      })
+      .catch((err) => {
+        logger.log(logger.ERROR, err, JSON.stringify(err));
+        res.writeHead(404, { 'Content-Type': 'text/plain'});
+        res.write('Resource not found');
+        res.end();
+      });
+    return undefined;
+  });
+  return undefined;
 };
+
